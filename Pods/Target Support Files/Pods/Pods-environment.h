96f95afbae10673d55d669000ebd703f9e9fac2f
刘任驰 <276415743@qq.com>
2015-05-09 00:49:22 +0800

    Revert 3464880..abbbb17
    This rolls back to commit 346488069f67ac616bf6de17faf4602ca8beaa5f.

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,56 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AMap3DMap
+#define COCOAPODS_POD_AVAILABLE_AMap3DMap
+#define COCOAPODS_VERSION_MAJOR_AMap3DMap 2
+#define COCOAPODS_VERSION_MINOR_AMap3DMap 5
+#define COCOAPODS_VERSION_PATCH_AMap3DMap 0
+
+// AMapSearch
+#define COCOAPODS_POD_AVAILABLE_AMapSearch
+#define COCOAPODS_VERSION_MAJOR_AMapSearch 2
+#define COCOAPODS_VERSION_MINOR_AMapSearch 5
+#define COCOAPODS_VERSION_PATCH_AMapSearch 0
+
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 2
+#define COCOAPODS_VERSION_PATCH_Alamofire 1
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 1
+
+// PZPullToRefresh
+#define COCOAPODS_POD_AVAILABLE_PZPullToRefresh
+#define COCOAPODS_VERSION_MAJOR_PZPullToRefresh 0
+#define COCOAPODS_VERSION_MINOR_PZPullToRefresh 0
+#define COCOAPODS_VERSION_PATCH_PZPullToRefresh 6
+
+// SDWebImage
+#define COCOAPODS_POD_AVAILABLE_SDWebImage
+#define COCOAPODS_VERSION_MAJOR_SDWebImage 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage 2
+
+// SDWebImage/Core
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_Core
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Core 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Core 2
+
+// SwiftyJSON
+#define COCOAPODS_POD_AVAILABLE_SwiftyJSON
+#define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_MINOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_PATCH_SwiftyJSON 0
+
